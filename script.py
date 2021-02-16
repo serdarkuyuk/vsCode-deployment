2bd14b4194067ac9377273451baa47261f74d12f
serdarkuyuk <serdarkuyuk@gmail.com>
2021-02-16 15:23:51 -0500

    removed codes

--- script.py
+++ script.py
@@ -1,23 +1,7 @@
-import math
-import os
-import sys
-
 import requests
 
 
-# name = input("your name")
-# print("Hello ", name)
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_great):
-    greeting = "Hello, {}".format(who_to_great)
-    return greeting
-
-
-print(greet("World"))
-print(greet("Serdar"))
 r = requests.get("https://serdarkuyuk.netlify.app")
 print(r.status_code)
+
+print(r.ok)
